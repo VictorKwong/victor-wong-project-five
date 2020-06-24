cd1c0dd47182121c35fa84241d3a6921973bc88c
Victor Wong <victorkaihong@gmail.com>
2020-06-24 15:59:39 -0400

    Updates

---
+++ precache-manifest.4ccc3a2a2d6188916f6ce77c47743076.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b161d3dae78be63bf7f03e00d80694f9",
+    "url": "/victor-wong-project-five/index.html"
+  },
+  {
+    "revision": "6f7fe68b70fba5943244",
+    "url": "/victor-wong-project-five/static/css/main.a41bd603.chunk.css"
+  },
+  {
+    "revision": "378b84b059c450332bcf",
+    "url": "/victor-wong-project-five/static/js/2.118ee2db.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/victor-wong-project-five/static/js/2.118ee2db.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6f7fe68b70fba5943244",
+    "url": "/victor-wong-project-five/static/js/main.48beb8bc.chunk.js"
+  },
+  {
+    "revision": "9a02abdc7fab85ed337f",
+    "url": "/victor-wong-project-five/static/js/runtime-main.9f7f1305.js"
+  },
+  {
+    "revision": "4cb866f8672594bc900fc7b02b9cf2c9",
+    "url": "/victor-wong-project-five/static/media/bg.4cb866f8.jpg"
+  },
+  {
+    "revision": "48eb9f97cf05cbbe0ed6bed69639e30e",
+    "url": "/victor-wong-project-five/static/media/checkmark.48eb9f97.png"
+  },
+  {
+    "revision": "09a6ae937a6d9ef5cd10d132b59d6f5d",
+    "url": "/victor-wong-project-five/static/media/pokeball.09a6ae93.png"
+  },
+  {
+    "revision": "4f86398306224b4cb259b13abe54c9f1",
+    "url": "/victor-wong-project-five/static/media/pokemonRecovery.4f863983.mp3"
+  },
+  {
+    "revision": "40c4fb54a72fca2c5b3641791377f493",
+    "url": "/victor-wong-project-five/static/media/victoryGym.40c4fb54.mp3"
+  },
+  {
+    "revision": "1246aa3d6591cd098b0184a13165f6f0",
+    "url": "/victor-wong-project-five/static/media/victoryTrainer.1246aa3d.mp3"
+  },
+  {
+    "revision": "f8d8d361c988036e4a3695fbf524c001",
+    "url": "/victor-wong-project-five/static/media/victoryWild.f8d8d361.mp3"
+  },
+  {
+    "revision": "5382ec55fd2e8d53001dd28504552baf",
+    "url": "/victor-wong-project-five/static/media/vsGymLeaer.5382ec55.mp3"
+  },
+  {
+    "revision": "3f4536ddf9f662ec0cf4f6a902899492",
+    "url": "/victor-wong-project-five/static/media/vsTrainer.3f4536dd.mp3"
+  },
+  {
+    "revision": "2479443c74c5749f9c73243093903b27",
+    "url": "/victor-wong-project-five/static/media/vsWild.2479443c.mp3"
+  }
+]);
