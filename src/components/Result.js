f26abcb594cb7fc50820d166a3612a6eec09db7e
Victor Wong <victorkaihong@gmail.com>
2020-06-24 16:03:20 -0400

    third commit

--- src/components/Result.js
+++ src/components/Result.js
@@ -4,7 +4,7 @@ class Result extends Component{
     render(){
         return(
         <div>
-            { this.props.final === true ? <p>You Won the Battle!</p> : this.props.final === false ? <p>Opps..Try Again!</p> : null }
+            { this.props.final === true ? <p>You Won the Battle!</p> : this.props.final === false ? <p>Opps..Try Again!!</p> : null }
         </div>
         )
     }
