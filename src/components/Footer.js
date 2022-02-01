24b480eef1df9276b9c096c3da1a2f518d26ffb3
victorkaiwong <victorkaihong@gmail.com>
2022-01-31 19:17:03 -0500

    footer update

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -17,7 +17,7 @@ easterEgg = (e) => {
     render(){
         return(
             <footer className="footerPosition">
-                {!this.state.trigger ? <p onClick={this.easterEgg} className="footerButton">Copyright ⓒ 2020 Victor Wong</p>
+                {!this.state.trigger ? <p onClick={this.easterEgg} className="footerButton">Copyright ⓒ 2022 Victor Wong</p>
                  : <p className="footerButton">Thanks for Playing!^^ You will Always got my EasterEgg! <a href="https://github.com/VictorKwong/victor-wong-project-five">GitHub</a></p>}
             </footer>
         )
